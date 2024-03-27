c9d0183fdacfc6ff8c71d924a37f7febb9dfd4d8
pedrodruviaro <pedrodruviaro@gmail.com>
2024-03-27 10:57:59 -0300

    feat: added NuxtImg package and base hero structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>home page</h1>
+  <section class="spacer bg-brand-200">
+    <UiContainer>
+      <div>
+        <img src="" alt="" />
+        <h1>
+          Olá, eu sou o <strong>Pedro</strong>. Desenvolvedor
+          <strong>front-end</strong>
+        </h1>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable.
+        </p>
+      </div>
+
+      <div>
+        <NuxtImg src="/images/home-hero-frame.svg" />
+      </div>
+    </UiContainer>
+  </section>
 </template>
