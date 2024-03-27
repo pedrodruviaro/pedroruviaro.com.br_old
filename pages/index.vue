d3be5054732edaec0fa6bc76e4aae68b99c44ade
pedrodruviaro <pedrodruviaro@gmail.com>
2024-03-27 13:38:04 -0300

    feat: :sparkles: hero section on homepage and all images

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="spacer bg-brand-200">
-    <UiContainer>
+  <section class="spacer bg-brand-300">
+    <UiContainer class="grid gap-10 lg:grid-cols-[2fr_1fr]">
       <div>
-        <img src="" alt="" />
-        <h1>
+        <NuxtImg
+          src="/images/home-hero-profile.png"
+          width="100"
+          height="100"
+          alt="Pedro Ruviaro"
+        />
+        <h1 class="text-2xl sm:text-3xl md:text-5xl my-8 max-w-[20ch]">
           Olá, eu sou o <strong>Pedro</strong>. Desenvolvedor
           <strong>front-end</strong>
         </h1>
-        <p>
+        <p class="max-w-[60ch]">
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
@@ -19,7 +24,12 @@
       </div>
 
       <div>
-        <NuxtImg src="/images/home-hero-frame.svg" />
+        <NuxtImg
+          src="/images/home-hero-frame.svg"
+          alt="Grafismo"
+          aria-hidden="true"
+          class="mx-auto"
+        />
       </div>
     </UiContainer>
   </section>
